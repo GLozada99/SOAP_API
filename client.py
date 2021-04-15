5d1aa39bee663201bb8d08e62370c1807b5052f3
GLozada99 <20170511@ce.pucmm.edu.do>
2021-04-15 16:48:12 -0400

    Commit inicial

---
+++ client.py
@@ -0,0 +1,17 @@
+class Client():
+    def __init__(self,endpoint='http://localhost:7000/api/estudiante/'):
+        self.endpoint = endpoint
+        PARAMS = dict()
+    
+    def findAll(self):
+        pass
+
+    def find(self,id):
+        pass
+
+    def delete(self,id):
+        pass
+
+    def insert(self,id,name,major):
+        pass
+
