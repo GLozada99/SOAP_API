f1cd12ea4a9e38d02fd33bbfa42d541655603c4a
GLozada99 <20170511@ce.pucmm.edu.do>
2021-04-15 16:55:31 -0400

    Find and FindAll

--- client.py
+++ client.py
@@ -3,12 +3,18 @@ import requests
 class Client():
     def __init__(self,endpoint='http://localhost:7000/api/estudiante/'):
         self.endpoint = endpoint
-        PARAMS = dict()
+        self.params = dict()
     
     def findAll(self):
+        req = requests.get(url = self.endpoint, params = self.params)
+        data = req.json()
+        print(data)
         pass
 
     def find(self,id):
+        req = requests.get(url = self.endpoint+'/'+id, params = self.params)
+        data = req.json()
+        print(data)
         pass
 
     def delete(self,id):
