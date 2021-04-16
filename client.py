a42afabe781316111fb1634590692ee123d1e5b3
GLozada99 <20170511@ce.pucmm.edu.do>
2021-04-15 21:39:05 -0400

    Finalizado

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 import json
-from suds.client import Client as CLI
-from student import Student
+from zeep.client import Client as CLI
+from student import Estudiante
 
 class Client():
     def __init__(self,endpoint='http://localhost:7000/ws/EstudianteWebServices?wsdl'):
@@ -18,12 +18,11 @@ class Client():
         print(student)
 
     def delete(self,id):
-        req = requests.delete(url = self.endpoint+id)
+        self.cli.service.borrarEstudiante(id)
         
     def insert(self,id,name,major):
         data = {'matricula': id, 'nombre': name, 'carrera': major}
         js = json.dumps(data)
-        
-        req = requests.post(url = self.endpoint, data = js)
+        self.cli.service.crearEstudiante(json.loads(str(js)))
         
 
