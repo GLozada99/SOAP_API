7057daefae30cdaca00b11bd22e2f2e3edc05dfe
GLozada99 <20170511@ce.pucmm.edu.do>
2021-04-15 21:22:13 -0400

    aaa

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from suds.client import Client as CLI
+from student import Student
 
 class Client():
     def __init__(self,endpoint='http://localhost:7000/ws/EstudianteWebServices?wsdl'):
@@ -8,15 +9,13 @@ class Client():
         self.cli = CLI(self.endpoint)
     
     def findAll(self):
-        req = requests.get(url = self.endpoint)
-        data = req.json()
-        for j in data:
-            print(j)
+        students = self.cli.service.getListaEstudiante()
+        for stu in students:
+            print(stu)
 
     def find(self,id):
-        req = requests.get(url = self.endpoint+id)
-        data = req.text
-        print(data)
+        student = self.cli.service.getEstudiante(id)
+        print(student)
 
     def delete(self,id):
         req = requests.delete(url = self.endpoint+id)
@@ -24,6 +23,7 @@ class Client():
     def insert(self,id,name,major):
         data = {'matricula': id, 'nombre': name, 'carrera': major}
         js = json.dumps(data)
+        
         req = requests.post(url = self.endpoint, data = js)
         
 
---
+++ student.py
@@ -0,0 +1,5 @@
+class Estudiante:
+    def __init__(self, nombre, matricula, carrera):
+        self.nombre = nombre
+        self.matricula = matricula
+        self.carrera = carrera
